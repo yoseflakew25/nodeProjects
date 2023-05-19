fe27ef3c35c5aeac125d58ce30674094abf2d5fa
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-19 15:23:57 +0300

    .

--- Simple Web Server/server.js
+++ Simple Web Server/server.js
@@ -4,7 +4,6 @@ const path = require('path');
 const fs = require('fs');
 
 
-
 const mimeTypes = {
     "html": "text/html",
     "jpeg": "image/jpeg",
@@ -14,3 +13,14 @@ const mimeTypes = {
     "js": "text/javascript",
     "json": "application/json",
 }
+
+
+http.createServer(function(req, res) {
+    const pathName = url.parse(req.url).pathname;
+    const filePath = path.join(__dirname, pathName);
+    const extname = path.extname(filePath);
+    const contentType = mimeTypes[extname] || "text/plain";
+    const content = fs.readFileSync(filePath);
+    res.writeHead(200, { 'Content-Type': contentType });
+    res.end(content);
+}).listen(8080);
