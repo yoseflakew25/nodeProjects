01cfba98881eab02a75897e77a77012446fc0f1e
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-19 15:47:08 +0300

    .

--- Simple Web Server/server.js
+++ Simple Web Server/server.js
@@ -14,34 +14,36 @@ const mimeTypes = {
     "json": "application/json",
 }
 http.createServer((req, res) => {
-            var uri = url.parse(req.url).pathname;
-            var fileName = path.join(process.cwd(), unescape(uri));
-            console.log("loading " + uri + "...");
-            var stats
+    var uri = url.parse(req.url).pathname;
+    var fileName = path.join(process.cwd(), unescape(uri));
+    console.log("loading " + uri + "...");
+    var stats
 
 
-            try {
-                stats = fs.lstatSync(fileName);
-            } catch (error) {
-                res.writeHead(404, { 'Content-type': 'text/plain' })
-                res.write('404 not found\n')
-                res.end()
-                return
-            }
+    try {
+        stats = fs.lstatSync(fileName);
+    } catch (error) {
+        res.writeHead(404, { 'Content-type': 'text/plain' })
+        res.write('404 not found\n')
+        res.end()
+        return
+    }
 
 
-            if (stats.isFile()) {
-                var mimeType = mimeTypes[path.extname(fileName).split('.').reverse()[0]]
-                res.writeHead(200, { 'Content-type': mimeType })
+    if (stats.isFile()) {
+        var mimeType = mimeTypes[path.extname(fileName).split('.').reverse()[0]]
+        res.writeHead(200, { 'Content-type': mimeType })
 
 
-                var fileStrem = fs.createReadStream(fileName);
-                fileStrem.pipe(res);
-            } else if (stats.isDirectory()) {
-                res.writeHead(302, { 'Location': 'index.html' })
-                res.end()
-            } else {
-                res.writeHead(500, { 'Content-type': 'text/plain' })
-                res.write('500 INternal error\n')
-                res.end()
-            }
+        var fileStrem = fs.createReadStream(fileName);
+        fileStrem.pipe(res);
+    } else if (stats.isDirectory()) {
+        res.writeHead(302, { 'Location': 'index.html' })
+        res.end()
+    } else {
+        res.writeHead(500, { 'Content-type': 'text/plain' })
+        res.write('500 INternal error\n')
+        res.end()
+    }
+
+}).listen(1337)
