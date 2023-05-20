3e538b73a53269a1b31b3cd0b601909349c90b56
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-20 20:53:41 +0300

    .

--- express_website/app.js
+++ express_website/app.js
@@ -10,3 +10,12 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000...');
+})
