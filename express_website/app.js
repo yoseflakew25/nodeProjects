dd6265135fb1dc09df53c6024eee7aece6e3b706
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-20 20:52:50 +0300

    .

--- express_website/app.js
+++ express_website/app.js
@@ -1,17 +1,11 @@
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
-var nodemailer = require('nodemailer');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const nodeMailer = require('nodemailer');
 
-var app = express();
+
+
+const app = express();
+
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.get('/', function(req, res) {
-    res.send('<h1>Hello World</h1>');
-});
-
-app.listen(3000);
-console.log('Server is running on port 3000...');
-a
