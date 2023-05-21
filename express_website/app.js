0fe79c4d41527c12f968bf2fdffe459da9110f8f
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-21 10:10:55 +0300

    .

--- express_website/app.js
+++ express_website/app.js
@@ -10,6 +10,7 @@ const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use(bodyParser.json());
